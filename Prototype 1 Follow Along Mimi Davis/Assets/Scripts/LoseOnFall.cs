094fdcab4add236431a8f2086be2ac72a23a2948
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-04 10:59:28 -0500

    prototype fixed

---
+++ Prototype 1 Follow Along Mimi Davis/Assets/Scripts/LoseOnFall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoseOnFall : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1)
+        {
+            ScoreManager.gameOver = true;
+        }
+    }
+}
