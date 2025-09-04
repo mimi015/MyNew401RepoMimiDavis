594641ee92c1aed48c1527b59147e0e800161588
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-04 12:29:37 -0500

    finished challenge 1

---
+++ Prototype 1 Follow Along Mimi Davis/Assets/Challenge 1/Scripts/LoseOnHit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoseOnHit : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 80f || transform.position.y < -51f)
+        {
+            ScoreManager2.gameOver = true;
+        }
+    }
+}
---
+++ Prototype 1 Follow Along Mimi Davis/Assets/Challenge 1/Scripts/TriggerOnceHit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOnceHit : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreManager2.score++;
+        }
+    }
+}
