129146d4e6c11234acdde2b144f38c5b43b735d7
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-10 19:20:12 -0500

    finished proto2

---
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottomBound = -10;
+
+    private HealthSystem healthSystemScript;
+    
+    private void Start()
+    {
+        healthSystemScript = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < bottomBound)
+        {
+            //Debug.Log("Game Over!");
+            healthSystemScript.TakeDamage();
+            Destroy(gameObject);
+        }
+    }
+}
