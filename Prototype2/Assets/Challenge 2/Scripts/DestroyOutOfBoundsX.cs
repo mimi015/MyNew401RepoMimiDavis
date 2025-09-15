7dcf08d88e45f5b23749921342f4b8f1617f0907
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-14 20:08:54 -0500

    finished challenge 2

---
+++ Prototype2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30;
+    private float bottomLimit = -5;
+
+    private HealthSystem2 healthSystemScript;
+
+    private void Start()
+    {
+        healthSystemScript = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem2>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            healthSystemScript.TakeDamage();
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Prototype2/Assets/Challenge 2/Scripts/ScoreManager.cs
+++ Prototype2/Assets/Challenge 2/Scripts/ScoreManager.cs
@@ -4,55 +4,46 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class DisplayScore : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
     public static bool gameOver;
     public static bool won;
-    
-    public Text textbox;
-    public int score = 0;
+    public static int score;
 
-    public int health = 5;
+    public Text textbox;
 
     // Start is called before the first frame update
     void Start()
     {
-        textbox = GetComponent<Text>();
-        textbox.text = "Score: 0";
         gameOver = false;
         won = false;
         score = 0;
-        health = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        textbox.text = "Score: " + score;
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
         if (score >=5)
         {
             won = true;
             gameOver = true;
         }
 
-        if (health ==0)
+        if (gameOver)
         {
-            gameOver = true;
-        }
-
-        if (won)
+            if (won)
             {
                 textbox.text = "You win!\nPress R to Try Again!";
             }
-        
-        if (gameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.R))
+             if (Input.GetKeyDown(KeyCode.R))
             {
-                textbox.text = "You lose!\nPress R to Try Again!";
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
-
     }
 }
