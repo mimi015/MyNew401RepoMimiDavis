c22a5ce35a4d889043c5e3effcafe66505fc8948
mimi015 <mdavi5086@365.elmhurst.edu>
2025-09-27 18:31:48 -0500

    finished

---
+++ Prototype 3/Assets/Challenge 3/Scripts/TriggerZoneAddScoreX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* Mimi Davis
+* TriggerZoneAddScoreX
+* Challenge3
+* Code makes the triggerzone add a score to the new text at the top of the scene
+*/
+
+public class TriggerZoneAddScoreX : MonoBehaviour
+{
+    private UIManagerX uIManager;
+
+    private bool triggered = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uIManager = GameObject.FindObjectOfType<UIManagerX>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true; 
+            uIManager.score++;
+        }
+    }
+}
---
+++ Prototype 3/Assets/Challenge 3/Scripts/UIManagerX.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+/*
+* Mimi Davis
+* UIManagerX
+* Challenge3
+* The code establishes the win and lose conditions
+*/
+
+public class UIManagerX : MonoBehaviour
+{
+    public Text scoreText;
+    public int score = 0;
+
+    public PlayerControllerX playerControllerScript;
+
+    public bool won = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+
+        if (playerControllerScript == null)
+        {
+            playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+        }
+
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerControllerScript.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+
+        if (playerControllerScript.gameOver && !won)
+        {
+            scoreText.text = "You Lose!\nPress R to Try Again!";
+        }
+
+        if (score >= 20)
+        {
+            playerControllerScript.gameOver = true;
+            won = true;
+
+            scoreText.text = "You Win!\nPress R to Try Again!";
+        }
+
+        if (playerControllerScript.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
