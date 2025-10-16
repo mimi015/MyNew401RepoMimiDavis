de569b45627013f77057a1c0f9b9cd41734dd02b
mimi015 <mdavi5086@365.elmhurst.edu>
2025-10-15 20:10:38 -0500

    probuilder

--- 3D Game Prototype/Assets/Scripts/ShootWithRaycasts.cs
+++ 3D Game Prototype/Assets/Scripts/ShootWithRaycasts.cs
@@ -8,6 +8,8 @@ public class ShootWithRaycasts : MonoBehaviour
     public  float damage = 10f;
     public float range = 100f;
     public Camera cam;
+    public ParticleSystem muzzleFlash;
+    public float hitForce = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +28,8 @@ public class ShootWithRaycasts : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
+
         RaycastHit hitInfo;
 
         if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
@@ -37,6 +41,11 @@ public class ShootWithRaycasts : MonoBehaviour
             if (target != null)
             {
                 target.TakeDamage(damage);
+
+                if (hitInfo.rigidbody != null)
+                {
+                    hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+                }
             }
         }
     }
